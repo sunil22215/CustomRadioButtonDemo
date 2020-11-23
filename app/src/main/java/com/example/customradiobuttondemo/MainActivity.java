bfc1616f64c4b107ec0e73007e2d9a388da59d05
sunil <sunilkhedkar22215@gmail.com>
2020-11-23 14:57:33 +0530

    add onclicklistener on radiogroup

--- app/src/main/java/com/example/customradiobuttondemo/MainActivity.java
+++ app/src/main/java/com/example/customradiobuttondemo/MainActivity.java
@@ -1,8 +1,6 @@
 package com.example.customradiobuttondemo;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
@@ -11,7 +9,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button button;
     private RadioButton genderradioButton;
     private RadioGroup radiogroup;
 
@@ -19,16 +16,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        radiogroup = (RadioGroup) findViewById(R.id.radioGroup);
+        RadioGroup radiogroup = (RadioGroup) findViewById(R.id.radioGroup);
+        radiogroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                int selectedId = radiogroup.getCheckedRadioButtonId();
+                genderradioButton = (RadioButton) findViewById(selectedId);
+                if (selectedId == -1) {
+                    Toast.makeText(MainActivity.this, "Nothing selected", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(MainActivity.this, genderradioButton.getText(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 
-    public void onclickbuttonMethod(View v) {
-        int selectedId = radiogroup.getCheckedRadioButtonId();
-        genderradioButton = (RadioButton) findViewById(selectedId);
-        if (selectedId == -1) {
-            Toast.makeText(MainActivity.this, "Nothing selected", Toast.LENGTH_LONG).show();
-        } else {
-            Toast.makeText(MainActivity.this, genderradioButton.getText(), Toast.LENGTH_LONG).show();
-        }
-    }
+
 }
+//    public void onclickbuttonMethod(View v) {
+
+//    }
